cef352878dffc3f50e267f03e11657ed5c573b47
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2022-12-05 22:49:19 +0530

    permission

--- app/src/main/java/com/example/oncesecond/PickPlace.java
+++ app/src/main/java/com/example/oncesecond/PickPlace.java
@@ -1,14 +1,27 @@
 package com.example.oncesecond;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class PickPlace extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class PickPlace extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_place);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.pick_map);
+        mapFragment.getMapAsync(this);
+//        AIzaSyBAu6XWebuZ5X2STwG-WqDZTuhhNqQipIY
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
