ce8d0f555e200e3fcddb9e39439f8e18ee99cfe9
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2022-12-05 13:45:34 +0530

    maps

---
+++ app/src/main/java/com/example/oncesecond/PickPlace.java
@@ -0,0 +1,14 @@
+package com.example.oncesecond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_place);
+    }
+}
