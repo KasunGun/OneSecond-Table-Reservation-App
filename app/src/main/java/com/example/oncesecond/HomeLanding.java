00cf8039e48abf953d4f335a1fd1a8efe76492a2
KasunGunathilake <wdgkasun@gmail.com>
2022-12-06 01:17:38 +0530

    home landing

---
+++ app/src/main/java/com/example/oncesecond/HomeLanding.java
@@ -0,0 +1,14 @@
+package com.example.oncesecond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_landing);
+    }
+}
