c5ed5ded04d8a16070046b5193907fcf5361bba5
KasunGunathilake <wdgkasun@gmail.com>
2022-12-07 21:41:00 +0530

    landing page

--- app/src/main/java/com/example/oncesecond/HomeLanding.java
+++ app/src/main/java/com/example/oncesecond/HomeLanding.java
@@ -1,14 +1,50 @@
 package com.example.oncesecond;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.oncesecond.databinding.ActivityHomeLandingBinding;
+
 public class HomeLanding extends AppCompatActivity {
+
+    ActivityHomeLandingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_landing);
+        binding = ActivityHomeLandingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+           switch (item.getItemId()){
+               case R.id.homeBottomNav:
+                   replaceFragment(new HomeFragment());
+                   break;
+               case R.id.finderBottomNav:
+                   replaceFragment(new FinderFragment());
+                   break;
+               case R.id.menuBottomNav:
+                   replaceFragment(new MenuFragment());
+                   break;
+               case R.id.hotelsBottomNav:
+                   replaceFragment(new HotelsFragment());
+                   break;
+           }
+
+           return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_navigation, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/oncesecond/Permission.java
+++ app/src/main/java/com/example/oncesecond/Permission.java
@@ -2,7 +2,10 @@ package com.example.oncesecond;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Permission extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Permission extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
+
+        final Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Permission.this, HomeLanding.class));
+            }
+        });
     }
 }
