a8bac7609882e610c1621923d844201ae5fd6788
KasunGunathilake <wdgkasun@gmail.com>
2022-12-09 14:15:08 +0530

    landing page map

--- app/src/main/java/com/example/oncesecond/Permission.java
+++ app/src/main/java/com/example/oncesecond/Permission.java
@@ -19,7 +19,7 @@ public class Permission extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Permission.this, HomeLanding.class));
+                startActivity(new Intent(Permission.this, PickPlaceLanding.class));
             }
         });
     }
