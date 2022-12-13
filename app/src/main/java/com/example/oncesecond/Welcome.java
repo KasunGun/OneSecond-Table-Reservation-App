ad80d07ef626eef13d049df682113ca8092f264f
KasunGunathilake <wdgkasun@gmail.com>
2022-12-13 12:53:17 +0530

    fingerprint auth

--- app/src/main/java/com/example/oncesecond/Welcome.java
+++ app/src/main/java/com/example/oncesecond/Welcome.java
@@ -5,34 +5,84 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
+import java.util.concurrent.Executor;
+
+@SuppressWarnings("deprecation")
 public class Welcome extends AppCompatActivity {
 
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        final Button signUpButton = findViewById(R.id.signUpButton);
-        final TextView loginNowButton = findViewById(R.id.loginButton);
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(getApplicationContext(), "Device Does not have Fingerprint", Toast.LENGTH_SHORT).show();
+                break; 
+                
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(getApplicationContext(), "Not Working", Toast.LENGTH_SHORT).show();
+                break;
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(getApplicationContext(), "No Fingerprint Assigned", Toast.LENGTH_SHORT).show();
+                break;
+                    
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(Welcome.this, executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
-            public void onClick(View v) {
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
 
-                Intent intent = new Intent(Welcome.this, Register.class);
-                startActivity(intent);
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Successfully Authenticated", Toast.LENGTH_SHORT).show();
+
+
+                final Button signUpButton = findViewById(R.id.signUpButton);
+                final TextView loginNowButton = findViewById(R.id.loginButton);
+
+                signUpButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(Welcome.this, Register.class);
+                        startActivity(intent);
+                    }
+                });
+
+                loginNowButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent login_intent = new Intent(Welcome.this, Login.class);
+                        startActivity(login_intent);
+                    }
+                });
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
             }
         });
 
-        loginNowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent login_intent = new Intent(Welcome.this, Login.class);
-                startActivity(login_intent);
-            }
-        });
+
     }
 }
